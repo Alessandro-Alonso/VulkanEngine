b065616c61c6465a6866a13f2fb2601dce7ecf4d
Alessandro Alonso <43657310+Alessandro-Alonso@users.noreply.github.com>
2025-12-27 21:24:36 +0100

    Update FileSystem.cpp

--- src/Vulkan/Platform/FileSystem.cpp
+++ src/Vulkan/Platform/FileSystem.cpp
@@ -1,4 +1,4 @@
-// This whole file is written by AI. I dind't want to waste my time writting this, and focus more on the internal parts of vulkan.
+// This whole file is written by AI. I dind't want to waste my time writting this, since i want to focus more on the internal parts of vulkan.
 // Este archivo entero fue escrito por la AI. No queria tirar mi tiempo escribiendo esto. Queria meterle mas focus a las partes 
 // internas de vulkan. Lo dejo aqui para quien lo vea, sepa que no es mi codigo.
 
